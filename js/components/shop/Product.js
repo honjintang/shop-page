5a563b82128cd7564555e6a4c7fb51f5bb97d820
Vivien Tang <vivientang@deloitte.co.uk>
2020-03-15 23:44:21 +0000

    add heart svg for product likes

--- js/components/shop/Product.js
+++ js/components/shop/Product.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import Card from 'react-bootstrap/Card'
+import heart from '../../../assets/images/heart-outline.svg'
 
 const Product = ({id, img, title, brand, price, size, sold }) => {
     return (
@@ -7,6 +8,11 @@ const Product = ({id, img, title, brand, price, size, sold }) => {
             <Card.Img className={sold ? 'card-img-sold' : null} variant="top" src={img} />
             {sold && <Card.ImgOverlay>
                 <Card.Title>SOLD</Card.Title>
+            </Card.ImgOverlay>}
+            {<Card.ImgOverlay>
+                <Card.Title className='like'>
+                    <img className='heart' src={heart} width="22.5px" height="22.5px"/>
+                </Card.Title>
             </Card.ImgOverlay>}
             <Card.Body>
             <Card.Title>{title}</Card.Title>
